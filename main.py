76dfe0b74472668ab9fc88d55114014f47f2d096
Einars <einarsskutans@gmail.com>
2024-11-05 21:20:04 +0200

    huh

--- main.py
+++ main.py
@@ -27,11 +27,28 @@ class Scraper:
             rawXML = requests.get(host.url).content
             self.ScrapedDataList.append(rawXML)
         return self.ScrapedDataList
+    
+class Formatter:
+    def __init__(self, scraper):
+        self.scraper = scraper
+    def filterArticles(self):
+        articles = []
+        EventHostList = scraper.EventHostList
+        structXML = BeautifulSoup(scraper.ScrapedDataList[0], "html.parser")
+        rawArticles = structXML.find_all(EventHostList[0].tag, class_=EventHostList[0].class_)
+        for i in rawArticles:
+            articles.append(i.get_text().strip())
 
 if __name__ == "__main__":
     print("Running as main")
     scraper = Scraper()
-    
+    formatter = Formatter(scraper=scraper)
+
+    scraper.ScrapeList() # This happens before formatting (procedures instead of functions)
+    print(scraper.ScrapedDataList)
+    print(formatter.filterArticles())
+
+"""
     rawXML = scraper.ScrapeList()
     structXML = BeautifulSoup(rawXML[0], "html.parser")
     rawArticles = structXML.find_all("h3", class_="entry-title")
@@ -40,3 +57,4 @@ if __name__ == "__main__":
         articles.append(i.get_text().strip())
     print(articles)
     
+"""
