5aa41db3f90a7ccd656bf3bbc6c3c7b14f5f0c5c
Einars <einarsskutans@gmail.com>
2024-11-06 17:51:21 +0200

    class print redefine

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ class EventHost:
         self.class_ = class_
 
 class Event:
-    def __init__(self, name, description, url):
+    def __init__(self, name, url, host):
         self.name = name
-        self.description = description
         self.url = url
+        self.host = host
+    def __str__(self):
+        return f"\n{self.name}\n{self.url}\n{self.host}"
 
 class Scraper:
     ScrapedDataList = []
@@ -37,7 +39,8 @@ class Formatter:
         structXML = BeautifulSoup(scraper.ScrapedDataList[0], "html.parser")
         rawArticles = structXML.find_all(EventHostList[0].tag, class_=EventHostList[0].class_)
         for i in rawArticles:
-            articles.append(i.get_text().strip())
+            articles.append(Event(i.get_text().strip(), "/url/", EventHostList[0].name))
+        return articles
 
 if __name__ == "__main__":
     print("Running as main")
@@ -45,16 +48,6 @@ if __name__ == "__main__":
     formatter = Formatter(scraper=scraper)
 
     scraper.ScrapeList() # This happens before formatting (procedures instead of functions)
-    print(scraper.ScrapedDataList)
-    print(formatter.filterArticles())
-
-"""
-    rawXML = scraper.ScrapeList()
-    structXML = BeautifulSoup(rawXML[0], "html.parser")
-    rawArticles = structXML.find_all("h3", class_="entry-title")
-    articles = []
-    for i in rawArticles:
-        articles.append(i.get_text().strip())
-    print(articles)
-    
-"""
+    events = formatter.filterArticles()
+    for i in events:
+        print(i)       
